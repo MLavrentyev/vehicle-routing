c1d0279ff8b422e59e6c723a54646bfc523476be
asvarga <alexander_varga@alumni.brown.edu>
2020-05-06 03:29:31 -0400

    node sizes depicting demand

--- src/vis.py
+++ src/vis.py
@@ -15,6 +15,8 @@ def plot(sol: VRPSolution):
         xs = [stop.x for stop in stops]
         ys = [stop.y for stop in stops]
         plt.plot(xs, ys)
+        ss = [stop.demand*10 for stop in stops]
+        plt.scatter(xs, ys, ss)
         plt.title(f"Feasible: {'??'} | Score: {'??'}")
     plt.draw()
     plt.pause(0.0001)   # whytf is this necessary?
