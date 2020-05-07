39c853e5d002d0060f880fe1ca9635431e02389a
Mark Lavrentyev <lavrema@outlook.com>
2020-05-06 22:38:45 -0400

    Refactor and add type hints

--- src/main.py
+++ src/main.py
@@ -8,25 +8,27 @@ from solver import VRPSolver
 
 from vis import plot, init
 
-### Run: python3 src/main.py simpleInput/21_4_1.vrp
+# Run: python3 src/main.py simpleInput/21_4_1.vrp
+
+def display():
+    print(f"sol\t\t: {sol} >> {sol.objectiveValue}")
+    plot(sol)
 
 if __name__ == '__main__':
     # print(f"sys.argv[1]: {sys.argv[1]}")
-    prob: VRPProblem = vrpIo.readInput(sys.argv[1])
-    print(f"problem: {prob}")
-
-    def display():
-        print(f"sol\t\t: {sol} >> {sol.objectiveValue}")
-        plot(sol)
+    problem: VRPProblem = vrpIo.readInput(sys.argv[1])
+    print(f"problem: {problem}")
 
     init()
-    sol: VRPSolution = VRPSolution.rand(prob).normalize()
+    sol: VRPSolution = VRPSolution.rand(problem).normalize()
     display()
+
     for _ in range(20):
         time.sleep(0.25)
-        neighs = sol.neighbors()+[sol]
-        neighs.sort(key=lambda n: n.objectiveValue)
-        best = neighs[0]
+        neighbs: List[VRPSolution] = sol.neighbors() + [sol]
+        neighbs.sort(key = (lambda n: n.distance))
+
+        best = neighbs[0]
         if best == sol:
             print("no better neighbor")
         else:
