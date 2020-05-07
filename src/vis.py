bef3a7760f1e4e31f387a1b60b61f306565efdef
Mark Lavrentyev <lavrema@outlook.com>
2020-05-06 22:27:01 -0400

    Clean up syntax of vis

--- src/vis.py
+++ src/vis.py
@@ -1,24 +1,22 @@
-
 import matplotlib.pyplot as plt
-
 from problem import VRPSolution
 
-####
 
 def init():
-    # plt.style.use('fivethirtyeight')
     plt.ion()
 
 def plot(sol: VRPSolution):
     for route in sol.routes:
-        stops = [sol.depot]+route.stops+[sol.depot]
+        stops = [sol.depot] + route.stops + [sol.depot]
         xs = [stop.x for stop in stops]
         ys = [stop.y for stop in stops]
         plt.plot(xs, ys)
-        ss = [stop.demand*10 for stop in stops]
+
+        ss = [stop.demand * 10 for stop in stops]
         plt.scatter(xs, ys, ss)
         plt.title(f"Feasible: {'??'} | Score: {'??'}")
+
     plt.draw()
-    plt.pause(0.0001)   # whytf is this necessary?
+    plt.pause(0.0001)
     plt.clf()
 
