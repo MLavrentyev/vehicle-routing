d98e2740d63269443d6c29be5969abb9f9c9ec27
Mark Lavrentyev <lavrema@outlook.com>
2020-05-08 16:32:10 -0400

    Add sectored initial solution

--- src/solver.py
+++ src/solver.py
@@ -44,7 +44,9 @@ class Solver(ABC):
 class VRPSolver(Solver):
 
     def solve(self, display: bool = False, maximizeObjV: bool = True) -> VRPSolution:
-        currState: VRPSolution = self.pickInitSolution()
+        doRandJumpProb: float = 0.1
+
+        currState: VRPSolution = self.pickSectoredSolution()
         improveCheck: Callable[[float, float], bool] = (lambda o, c: o > c) if maximizeObjV else (lambda o, c: o < c)
 
         if display:
@@ -54,41 +56,86 @@ class VRPSolver(Solver):
         done: bool = False
         while not done:
             done = True
-            # II - look for better neighbor (if none, we are done)
-            neighbSolution: VRPSolution
-            for neighbSolution in self.neighborhood(currState):
-                if improveCheck(neighbSolution.objectiveValue, currState.objectiveValue):
-                    currState = neighbSolution
+
+            # do random jump with some probability
+            if random.random() <= doRandJumpProb:
+                currState = currState.neighbors().__next__()
+                done = False
+            else:
+                # iterative improvement - look for better neighbor (if none, we are done)
+                neighbSolution: VRPSolution
+                for neighbSolution in currState.neighbors():
+                    if improveCheck(neighbSolution.objectiveValue, currState.objectiveValue):
+                        currState = neighbSolution
+                        done = False
+                        break
+
+                # if the current state is infeasible and we didn't improve this round, randomly pick a neighbor
+                if not currState.isFeasible() and done:
+                    currState = currState.neighbors().__next__()
                     done = False
-                    break
 
             if display and not done:
-                vis.display(currState)
+                vis.display(currState, doPlot=False)
 
         return currState
 
-    def neighborhood(self, solution: Solution) -> Generator[VRPSolution, None, None]:
-        solution = cast(VRPSolution, solution)
-
-        for neighbor in solution.neighbors():
-            yield neighbor
-
-    def pickInitSolution(self) -> VRPSolution:
+    def pickRandomSolution(self) -> VRPSolution:
         problem = cast(VRPProblem, self.problem)
 
         nodes = problem.nodes[:]
         random.shuffle(nodes)
 
-        routes = []
+        routes: List[Route] = []
         startIdx = 0
-        for _ in range(problem.numTrucks):
-            endIdx = random.randint(0, problem.numCustomers - startIdx)
+        for _ in range(problem.numTrucks - 1):
+            endIdx = random.randint(startIdx, problem.numCustomers)
             routes.append(Route(nodes[startIdx:endIdx], problem.depot))
             startIdx = endIdx
 
+        routes.append(Route(nodes[startIdx:], problem.depot))
+
         return VRPSolution(problem, routes)
 
+    def pickSectoredSolution(self) -> VRPSolution:
+        # Pick an initial solution where each truck is limited to routing among nodes in a given pie slice
+        problem = cast(VRPProblem, self.problem)
+
+        # slices divided ccw starting from positive x-axis (where depot is origin)
+        sliceSizeRad: float = (2 * math.pi) / problem.numTrucks
+
+        sectors: List[List[Node]] = [[] for _ in range(problem.numTrucks)]
+        node: Node
+        for node in problem.nodes:
+            # angle from 0 to 2pi
+            nodeAngle: float = math.atan2(node.y - problem.depot.y, node.x - problem.depot.x) + math.pi
+            sectorIdx: int = math.floor(nodeAngle / sliceSizeRad)
+            sectors[sectorIdx].append(node)
+
+        # Generate greedy routing for each sector
+        routes: List[Route] = []
+        sector: List[Node]
+        for sector in sectors:
+            route: Route = Route(([sector.pop()] if sector else []), problem.depot)
+
+            while sector:
+                closestIdx: int = 0
+                minDist: float = sector[closestIdx].distance(route.stops[-1])
+                for n in range(len(sector)):
+                    nodeDist: float = sector[n].distance(route.stops[-1])
+                    if nodeDist < minDist:
+                        closestIdx = n
+                        minDist = nodeDist
+                route.addStop(sector.pop(closestIdx))
+
+            routes.append(route)
+
+        return VRPSolution(problem, routes)
+
+
     def pickAnySolution(self) -> VRPSolution:
+        problem: VRPProblem = cast(VRPProblem, self.problem)
+
         indss = [range(i, len(problem.nodes), problem.numTrucks) for i in range(problem.numTrucks)]
 
         return VRPSolution(problem, [Route([problem.nodes[i] for i in inds], problem.depot) for inds in indss])
