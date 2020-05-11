cd28b965bece5439b17c1ec2f7117c66e49265dc
Mark Lavrentyev <lavrema@outlook.com>
2020-05-10 22:52:23 -0400

    Add option for running pypy in msys on windows

--- run.sh
+++ run.sh
@@ -6,21 +6,25 @@
 E_BADARGS=65
 if [ $# -ne 1 ]
 then
-	echo "Usage: $(basename "$0") <input>"
-	exit $E_BADARGS
+  echo "Usage: $(basename "$0") <input>"
+  exit $E_BADARGS
 fi
-	
+
 input=$1
 
 #java -cp src solver.ls.Main $input
 
 if hash pypy3 2>/dev/null; then
-	echo "using pypy3"
-	pypy3 src/solver.py "$input"
+  echo "using pypy3"
+  if [[ "$OSTYPE" == "msys" ]]; then
+    winpty pypy3 src/solver.py "$input"
+  else
+    pypy3 src/solver.py "$input"
+  fi
 elif hash pypy3.6-v7.1.1 2>/dev/null; then
-	echo "using pypy3.6-v7.1.1"
-	pypy3.6-v7.1.1 src/solver.py "$input"
+  echo "using pypy3.6-v7.1.1"
+  pypy3.6-v7.1.1 src/solver.py "$input"
 else
-	echo "using python"
-	python src/solver.py "$input"
+  echo "using python"
+  python src/solver.py "$input"
 fi
